b0628dba3c58203324f100965df5888c6b3c044c
yeahboy92 <yeahboy92@gmail.com>
2020-06-03 00:39:03 -0500

    Recreated Repository. I hate this. It isn't intuitive.

---
+++ Scripts/Player01/Player01.gd
@@ -0,0 +1,50 @@
+extends KinematicBody2D
+
+var motion = Vector2()
+var speed = 200
+var facing = 0
+var isMovingX = false
+var isMovingY = false
+
+func _physics_process(delta):
+	
+	# adds x motion to player
+	if Input.is_action_pressed("ui_right"):
+		motion.x = speed
+		isMovingX = true
+		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.play("walkRight")
+		
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = -speed
+		isMovingX = true
+		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.play("walkRight")
+		
+	else:
+		motion.x = 0
+		isMovingX = false
+		
+		
+	# adds y motion to player
+	if Input.is_action_pressed("ui_down"):
+		motion.y = speed
+		isMovingY = true
+		$AnimatedSprite.play("walkDown")
+		
+	elif Input.is_action_pressed("ui_up"):
+		motion.y = -speed
+		isMovingY = true
+		$AnimatedSprite.play("walkUp")
+		
+	else:
+		motion.y = 0
+		isMovingY = false
+	
+	if isMovingX == false && isMovingY == false:
+		$AnimatedSprite.stop()
+		$AnimatedSprite.set_frame(0)
+	
+	motion = move_and_slide(motion)
+
+	pass
