9d316cfa2bfa441069e6a682bba5f3e25be57e52
Greel70 <m_hoppe@live.com>
2020-06-04 15:20:43 -0500

    Modified Main Menu and mapped the new game button to start the level 1 test scene.

--- Scripts/UI/MainMenu.gd
+++ Scripts/UI/MainMenu.gd
@@ -1,12 +1,9 @@
-extends Node
+extends PanelContainer
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-onready var continue_Label = $HBoxContainer/VBoxContainer/MenuOptions/Continue
-onready var new_Game_Label = $HBoxContainer/VBoxContainer/MenuOptions/NewGame
-onready var options_Label = $HBoxContainer/VBoxContainer/MenuOptions/Options
 
 
 # Called when the node enters the scene tree for the first time.
@@ -19,13 +16,6 @@ func _ready():
 #	pass
 
 
-
-
-func _on_Continue_mouse_entered():
-	continue_Label.set("custom_colors/font_color", Color( 1, 1, 0, 1 ))
-	pass
-
-
-func _on_Continue_mouse_exited():
-	continue_Label.set("custom_colors/font_color", Color( 1, 1, 1, 1 ))
-	pass
+func _on_NewGameButton_pressed():
+	get_tree().change_scene("res://Scenes/TestLevelOne.tscn")
+	pass # Replace with function body.
